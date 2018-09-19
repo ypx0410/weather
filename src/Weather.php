b0e6f333f77eb21031d2e023878329220ae3aad8
ypx <ypx1143785498@qq.com>
2018-09-19 09:21:09 +0000

    Apply fixes from StyleCI

--- src/Weather.php
+++ src/Weather.php
@@ -1,5 +1,16 @@
 <?php
+
+/*
+ * This file is part of the ypx/weather.
+ *
+ * (c) ypx<1143785498@qq.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 namespace Ypx\Weather;
+
 use GuzzleHttp\Client;
 use Ypx\Weather\Exceptions\HttpException;
 use Ypx\Weather\Exceptions\InvalidArgumentException;
@@ -7,6 +18,7 @@ use Ypx\Weather\Exceptions\InvalidArgumentException;
 class Weather
 {
     protected $key;
+
     protected $guzzleOptions = [];
 
     public function __construct($key)
@@ -30,7 +42,7 @@ class Weather
 
         // 1. 对 `$format` 与 `$extensions` 参数进行检查，不在范围内的抛出异常。
         if (!\in_array($format, ['xml', 'json'])) {
-            throw new InvalidArgumentException('Invalid response format: '. $format);
+            throw new InvalidArgumentException('Invalid response format: '.$format);
         }
 
         if (!\in_array(\strtolower($type), ['base', 'all'])) {
@@ -54,7 +66,7 @@ class Weather
 
             // 4. 返回值根据 $format 返回不同的格式，
             // 当 $format 为 json 时，返回数组格式，否则为 xml。
-            return $format === 'json' ? \json_decode($response, true) : $response;
+            return 'json' === $format ? \json_decode($response, true) : $response;
         } catch (\Exception $e) {
             // 5. 当调用出现异常时捕获并抛出，消息为捕获到的异常消息，
             // 并将调用异常作为 $previousException 传入。
